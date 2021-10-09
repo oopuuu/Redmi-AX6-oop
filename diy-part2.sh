154b6c7ca21dd7f2cb59b2690ef6c8ad7a0df6f0
oopuuu <75833466+oopuuu@users.noreply.github.com>
2021-10-09 14:51:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,6 +22,9 @@ sed -i 's/root::0:0:99999:7:::/root:$1$wFECtu7f$TN5VYXH0jHoWEH0ing1pY.:18888:0:9
 # Modify Web UI Theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
+# Modify connection number
+echo net.netfilter.nf_conntrack_max=65535 >> package/base-files/files/etc/sysctl.conf
+
 # Modify wifi Name & Password
 sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio0.ssid=Redmi_1394_5G\n			set wireless.default_radio1.ssid=Redmi_1394/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/set wireless.default_radio${devidx}.encryption=none/set wireless.default_radio${devidx}.encryption=sae-mixed\n			set wireless.default_radio${devidx}.key=zp961203/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
